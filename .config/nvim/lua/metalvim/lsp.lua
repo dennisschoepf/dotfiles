af264ebd11c8e3e37ba867975d149800d3476ee5
Dennis Schoepf <me@dnsc.io>
2022-04-06 18:28:59 +0200

    Fixes go-to-definition for tsserver

--- .config/nvim/lua/metalvim/lsp.lua
+++ .config/nvim/lua/metalvim/lsp.lua
@@ -38,6 +38,25 @@ local function lsp_keymaps(_, bufnr)
 	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+local function filter(arr, fn)
+	if type(arr) ~= "table" then
+		return arr
+	end
+
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
+
+	return filtered
+end
+
+local function filterReactDTS(value)
+	return string.match(value.uri, "react/index.d.ts") == nil
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
@@ -57,6 +76,18 @@ local enhance_server_opts = {
 					globals = { "vim" },
 				},
 			},
+		}
+	end,
+	["tsserver"] = function(tsopts)
+		tsopts.handlers = {
+			["textDocument/definition"] = function(err, result, method, ...)
+				if vim.tbl_islist(result) and #result > 1 then
+					local filtered_result = filter(result, filterReactDTS)
+					return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+				end
+
+				vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+			end,
 		}
 	end,
 }
