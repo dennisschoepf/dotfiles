0099e8e5885ddefff06118f0941632015686e7c5
Dennis Schoepf <me@dnsc.io>
2022-04-29 12:45:56 +0200

    Uses native fzf for telescope

--- .config/nvim/lua/metalvim/plugins.lua
+++ .config/nvim/lua/metalvim/plugins.lua
@@ -50,6 +50,7 @@ return require("packer").startup(function(use)
 
 	-- Telescope
 	use("nvim-telescope/telescope.nvim")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use("nvim-telescope/telescope-file-browser.nvim")
 
 	-- Completion
--- .config/nvim/lua/metalvim/telescope.lua
+++ .config/nvim/lua/metalvim/telescope.lua
@@ -1,13 +1,14 @@
-require('telescope').setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-h>"] = "which_key"
-      }
-    }
-  },
-  pickers = {},
-  extensions = {}
-}
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-h>"] = "which_key",
+			},
+		},
+	},
+	pickers = {},
+	extensions = {},
+})
 
-require("telescope").load_extension "file_browser"
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("file_browser")
