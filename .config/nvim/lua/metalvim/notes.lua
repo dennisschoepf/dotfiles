ae4b46f2726686cb4064b52e51410c097779020b
Dennis Schoepf <me@dnsc.io>
2022-06-12 23:02:42 +0200

    Starts neorg config

---
+++ .config/nvim/lua/metalvim/notes.lua
@@ -0,0 +1,18 @@
+require("neorg").setup({
+	load = {
+		["core.defaults"] = {},
+		["core.norg.concealer"] = {},
+		["core.norg.completion"] = {
+			config = {
+				engine = "nvim-cmp",
+			},
+		},
+		["core.norg.dirman"] = {
+			config = {
+				workspaces = {
+					main = "~/notes",
+				},
+			},
+		},
+	},
+})
