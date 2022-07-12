bf9b2f7f43f0744d6aae84bd421a95eb37e9d738
Dennis Schoepf <me@dnsc.io>
2022-07-12 09:58:14 +0200

    Updates null ls config

--- .config/nvim/lua/metalvim/null_ls.lua
+++ .config/nvim/lua/metalvim/null_ls.lua
@@ -3,7 +3,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prettier.with({
+			disabled_filetypes = { "json" },
+		}),
 	},
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
