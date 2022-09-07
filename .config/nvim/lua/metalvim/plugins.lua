b4d02e2e285f9ed3ce58e81a6b59157d6d6fd72d
Dennis Schoepf <me@dnsc.io>
2022-09-07 11:53:54 +0200

    Adapts colorscheme for terminal and neovim

--- .config/nvim/lua/metalvim/plugins.lua
+++ .config/nvim/lua/metalvim/plugins.lua
@@ -19,6 +19,7 @@ return require("packer").startup(function(use)
 
 	-- Colorscheme
 	use("EdenEast/nightfox.nvim")
+	use({ "projekt0n/github-nvim-theme" })
 
 	-- UI
 	use("nvim-lualine/lualine.nvim")
