4b9456ee24119113b37fe0338a7946bc636a75d1
Dennis Schoepf <me@dnsc.io>
2022-09-22 10:33:30 +0200

    Updates neovim bufferline plugin

--- .config/nvim/lua/metalvim/bufferline.lua
+++ .config/nvim/lua/metalvim/bufferline.lua
@@ -1,39 +1,183 @@
-require("bufferline").setup({
-	options = {
-		mode = "buffers", -- set to "tabs" to only show tabpages instead
-		numbers = "none",
-		close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-		middle_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-		buffer_close_icon = "",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-			if buf.name:match("%.md") then
-				return vim.fn.fnamemodify(buf.name, ":t:r")
-			end
-		end,
-		max_name_length = 18,
-		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-		tab_size = 18,
-		diagnostics = "nvim_lsp",
-		diagnostics_update_in_insert = false,
-		diagnostics_indicator = function(count, _, _, _) -- level, diagnostics_dict, context
-			return "(" .. count .. ")"
-		end,
-		color_icons = true, -- whether or not to add the filetype icon highlights
-		show_buffer_icons = true, -- disable filetype icons for buffers
-		show_buffer_close_icons = true,
-		show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
-		show_close_icon = true,
-		show_tab_indicators = true,
-		persist_buffer_sort = true,
-		separator_style = "thin",
-		enforce_regular_tabs = false,
-		always_show_bufferline = true,
-		sort_by = "insert_after_current",
-	},
+local get_hex = require('cokeline/utils').get_hex
+local mappings = require('cokeline/mappings')
+
+local comments_fg = get_hex('Comment', 'fg')
+local errors_fg = get_hex('DiagnosticError', 'fg')
+local warnings_fg = get_hex('DiagnosticWarn', 'fg')
+
+local red = vim.g.terminal_color_1
+local yellow = vim.g.terminal_color_3
+
+local components = {
+  space = {
+    text = ' ',
+    truncation = { priority = 1 }
+  },
+
+  empty_space = {
+    text = ' ',
+    bg = get_hex('Normal', 'bg'),
+  },
+
+  rounded_left = {
+    text = '',
+    bg = get_hex('Normal', 'bg'),
+    fg = function(buffer)
+      return
+        buffer.is_focused
+        and get_hex('Normal', 'fg')
+         or get_hex('ColorColumn', 'bg')
+    end
+  },
+
+  rounded_right = {
+    text = '',
+    bg = get_hex('Normal', 'bg'),
+    fg = function(buffer)
+      return
+        buffer.is_focused
+        and get_hex('Normal', 'fg')
+         or get_hex('ColorColumn', 'bg')
+    end
+  },
+
+  two_spaces = {
+    text = '  ',
+    truncation = { priority = 1 },
+  },
+
+  separator = {
+    text = function(buffer)
+      return buffer.index ~= 1 and '' or ''
+    end,
+    truncation = { priority = 1 }
+  },
+
+  devicon = {
+    text = function(buffer)
+      return
+        (mappings.is_picking_focus() or mappings.is_picking_close())
+          and buffer.pick_letter .. ' '
+           or buffer.devicon.icon
+    end,
+    fg = function(buffer)
+      return
+        (mappings.is_picking_focus() and yellow)
+        or (mappings.is_picking_close() and red)
+        or get_hex('Normal', 'bg')
+    end,
+    style = function(_)
+      return
+        (mappings.is_picking_focus() or mappings.is_picking_close())
+        and 'italic,bold'
+         or nil
+    end,
+    truncation = { priority = 1 }
+  },
+
+  index = {
+    text = function(buffer)
+      return buffer.index .. ': '
+    end,
+    truncation = { priority = 1 }
+  },
+
+  unique_prefix = {
+    text = function(buffer)
+      return buffer.unique_prefix
+    end,
+    fg = comments_fg,
+    style = 'italic',
+    truncation = {
+      priority = 3,
+      direction = 'left',
+    },
+  },
+
+  filename = {
+    text = function(buffer)
+      return buffer.filename
+    end,
+    style = function(buffer)
+      return
+        ((buffer.is_focused and buffer.diagnostics.errors ~= 0)
+          and 'bold,underline')
+        or (buffer.is_focused and 'bold')
+        or (buffer.diagnostics.errors ~= 0 and 'underline')
+        or nil
+    end,
+    truncation = {
+      priority = 2,
+      direction = 'left',
+    },
+  },
+
+  diagnostics = {
+    text = function(buffer)
+      return
+        (buffer.diagnostics.errors ~= 0 and '  ' .. buffer.diagnostics.errors)
+        or (buffer.diagnostics.warnings ~= 0 and '  ' .. buffer.diagnostics.warnings)
+        or ''
+    end,
+    fg = function(buffer)
+      return
+        (buffer.diagnostics.errors ~= 0 and errors_fg)
+        or (buffer.diagnostics.warnings ~= 0 and warnings_fg)
+        or nil
+    end,
+    truncation = { priority = 1 },
+  },
+
+  close_or_unsaved = {
+    text = function(buffer)
+      return buffer.is_modified and '●' or ''
+    end,
+    fg = function(buffer)
+      return buffer.is_modified and green or nil
+    end,
+    delete_buffer_on_left_click = true,
+    truncation = { priority = 1 },
+  },
+}
+
+require('cokeline').setup({
+  show_if_buffers_are_at_least = 2,
+
+  buffers = {
+    -- filter_valid = function(buffer) return buffer.type ~= 'terminal' end,
+    -- filter_visible = function(buffer) return buffer.type ~= 'terminal' end,
+    new_buffers_position = 'next',
+  },
+
+  rendering = {
+    max_buffer_width = 34,
+  },
+
+  default_hl = {
+    fg = function(buffer)
+      return
+        buffer.is_focused
+        and get_hex('Normal', 'bg')
+         or get_hex('Comment', 'fg')
+    end,
+    bg = function(buffer)
+      return
+        buffer.is_focused
+        and get_hex('Normal', 'fg')
+         or get_hex('ColorColumn', 'bg')
+    end,
+  },
+
+  components = {
+    components.empty_space,
+    components.rounded_left,
+    components.devicon,
+    components.space,
+    components.filename,
+    -- components.diagnostics,
+    components.two_spaces,
+    components.close_or_unsaved,
+    components.rounded_right,
+    components.empty_space,
+  },
 })
