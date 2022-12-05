2be8bc5aa0a05768227cf5585aad878ec24112e5
Dennis Schoepf <me@dnsc.io>
2022-12-05 11:57:40 +0100

    Updates neovim and tmux config

--- .config/nvim/lua/coding.lua
+++ .config/nvim/lua/coding.lua
@@ -151,7 +151,6 @@ require("lspconfig").sumneko_lua.setup({
 })
 
 -- Typescript
-
 require("typescript").setup({
 	disable_commands = false, -- prevent the plugin from creating Vim commands
 	debug = false, -- enable debug logging for commands
