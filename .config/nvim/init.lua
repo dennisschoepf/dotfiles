29e71a38389a31d893f5fe95cd50b757bb360065
Dennis Schoepf <me@dnsc.io>
2023-01-02 22:31:49 +0100

    Simplifies neovim config with lsp-zero and lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,162 @@
 --[[
-███╗   ███╗██╗███╗   ██╗██╗███╗   ███╗ █████╗ ██╗   ██╗██╗███╗   ███╗
-████╗ ████║██║████╗  ██║██║████╗ ████║██╔══██╗██║   ██║██║████╗ ████║
-██╔████╔██║██║██╔██╗ ██║██║██╔████╔██║███████║██║   ██║██║██╔████╔██║
-██║╚██╔╝██║██║██║╚██╗██║██║██║╚██╔╝██║██╔══██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚═╝ ██║██║██║ ╚████║██║██║ ╚═╝ ██║██║  ██║ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+███╗   ██╗██╗   ██╗██╗███╗   ███╗
+████╗  ██║██║   ██║██║████╗ ████║
+██╔██╗ ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
 
-Author: Dennis Schoepf
-Version: 0.0.1
+Config Author: Dennis Schoepf
+Version: 1.0.0
 ]]
 --
+
+--[[
+HELPER VARIABLES
+--]]
+local opts = { noremap = true, silent = true, expr = true }
 
 --[[
 GENERAL SETTINGS
 --]]
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-local opts = { noremap = true, silent = true, expr = true }
-
 vim.keymap.set("", "<Space>", "<Nop>", opts)
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", opts)
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", opts)
+
+--[[
+OPTIONS
+--]]
+local options = {
+	backup = false, -- creates a backup file
+	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+	cmdheight = 1, -- more space in the neovim command line for displaying messages
+	completeopt = { "menu", "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8", -- the encoding written to a file
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	mouse = "a", -- allow the mouse to be used in neovim
+	pumheight = 10, -- pop up menu height
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	smartindent = true,
+	breakindent = true,
+	showtabline = 0, -- always show tabs
+	smartcase = true, -- smart case
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	timeoutlen = 200, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true, -- enable persistent undo
+	updatetime = 230, -- faster completion (4000ms default)
+	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	tabstop = 2, -- insert 2 spaces for a tab
+	cursorline = true, -- highlight the current line
+	number = true, -- set numbered lines
+	relativenumber = true, -- set relative numbered lines
+	numberwidth = 4, -- set number column width to 2 {default 4}
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	linebreak = true,
+	breakat = " ^I!@*-+;:,./?",
+	scrolloff = 6, -- is one of my fav
+	sidescrolloff = 4,
+	winbar = nil,
+}
+
+vim.opt.shortmess:append("c")
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+--[[
+LAZY SETUP
+--]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+--[[
+PLUGINS
+--]]
+require("lazy").setup({
+	{ "catppuccin/nvim", name = "catppuccin" },
+	"kyazdani42/nvim-web-devicons",
+	"nvim-lua/plenary.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"romainl/vim-cool",
+	"echasnovski/mini.pairs",
+	"folke/which-key.nvim",
+	"romgrk/barbar.nvim",
+	"nvim-lualine/lualine.nvim",
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	"nvim-telescope/telescope-file-browser.nvim",
+	{ "nvim-telescope/telescope.nvim", version = "1.1.0" },
+	"windwp/nvim-ts-autotag",
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"lewis6991/gitsigns.nvim",
+	"kdheepak/lazygit.nvim",
+	-- Writing
+	{ "iamcco/markdown-preview.nvim", build = "cd app && npm install", ft = { "markdown" } },
+	"folke/zen-mode.nvim",
+	"dkarter/bullets.vim",
+	-- Coding
+	"jose-elias-alvarez/null-ls.nvim",
+	{ "Maan2003/lsp_lines.nvim", url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			-- LSP Support
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			-- Autocompletion
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			-- Snippets
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+})
+
+--[[
+SMALL HELPERS
+--]]
+local function filter(arr, fn)
+	if type(arr) ~= "table" then
+		return arr
+	end
+
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
+
+	return filtered
+end
+
+local function filterReactDTS(value)
+	return string.match(value.targetUri, "%.d.ts") == nil
+end
+
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -34,277 +168,221 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 --[[
-PLUGINS
+PLUGIN CONFIGURATION
 --]]
 
--- Install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	is_bootstrap = true
-	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-	vim.cmd([[packadd packer.nvim]])
-end
+-- COLORSCHEME
+require("catppuccin").setup()
+vim.api.nvim_command("colorscheme catppuccin")
 
-require("packer").startup(function(use)
-	-- COLORSCHEME
-	use({
-		"catppuccin/nvim",
-		as = "catppuccin",
-		config = function()
-			require("catppuccin").setup()
-			vim.api.nvim_command("colorscheme catppuccin")
+-- INDENT BLANKLINE
+require("indent_blankline").setup({})
+
+-- MINI PAIRS
+require("mini.pairs").setup()
+
+-- WHICH-KEY
+require("which-key").setup({
+	window = {
+		border = "single", -- none, single, double, shadow
+		position = "bottom",
+		margin = { 0, 0, 0, 0 },
+		padding = { 2, 1, 2, 1 },
+	},
+	layout = {
+		align = "center",
+	},
+})
+
+-- BUFFERLINE
+require("bufferline").setup({
+	animation = false,
+	auto_hide = false,
+	maximum_length = 35,
+	closable = false,
+	icons = false,
+})
+
+-- STATUSLINE
+require("lualine").setup({
+	options = {
+		icons_enabled = false,
+		-- globalstatus = true
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = {},
+		lualine_c = { "branch", "diff", "diagnostics" },
+		lualine_x = { "filetype", "encoding" },
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+})
+
+-- TELESCOPE
+require("telescope").setup({
+	file_sorter = "",
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+		git_files = {
+			theme = "ivy",
+		},
+		oldfiles = {
+			theme = "ivy",
+		},
+		live_grep = {
+			theme = "ivy",
+		},
+		buffers = {
+			theme = "ivy",
+		},
+		current_buffer_fuzzy_find = {
+			theme = "ivy",
+		},
+		lsp_references = {
+			theme = "ivy",
+		},
+		diagnostics = {
+			theme = "ivy",
+		},
+	},
+	extensions = {
+		file_browser = {
+			theme = "ivy",
+			hidden = false,
+		},
+		fzf = {
+			case_mode = "ignore_case",
+		},
+	},
+})
+
+require("telescope").load_extension("file_browser")
+require("telescope").load_extension("fzf")
+
+-- TREESITTER
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	auto_install = true,
+	ignore_install = { "javascript" },
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+})
+
+-- GITSIGNS
+require("gitsigns").setup({
+	signs = {
+		add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = {
+			hl = "GitSignsChange",
+			text = "│",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
+		delete = {
+			hl = "GitSignsDelete",
+			text = "_",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
+		},
+		topdelete = {
+			hl = "GitSignsDelete",
+			text = "‾",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
+		},
+		changedelete = {
+			hl = "GitSignsChange",
+			text = "~",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
+	},
+	signcolumn = true,
+	current_line_blame = false,
+})
+
+-- MARKDOWN-PREVIEW
+vim.g.mkdp_filetypes = { "markdown" }
+
+-- LSP-LINES
+require("lsp_lines").setup()
+vim.diagnostic.config({
+	virtual_text = false,
+	virtual_lines = false,
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
+})
+vim.keymap.set("", "<Leader>e", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+
+-- LSP-ZERO & NULL-LS
+local lsp = require("lsp-zero")
+local null_ls = require("null-ls")
+local null_opts = lsp.build_options("null-ls", {})
+
+lsp.preset("recommended")
+lsp.ensure_installed({
+	"tsserver",
+	"eslint",
+	"sumneko_lua",
+	"jsonls",
+	"marksman",
+	"html",
+})
+lsp.nvim_workspace()
+lsp.setup()
+
+-- TSSERVER CUSTOMIZATION
+lsp.configure("tsserver", {
+	handlers = {
+		["textDocument/definition"] = function(err, result, method, ...)
+			if vim.tbl_islist(result) and #result > 1 then
+				local filtered_result = filter(result, filterReactDTS)
+				return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+			end
+
+			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
 		end,
-	})
+	},
+})
 
-	-- PLUGIN MANAGER
-	use({ "wbthomason/packer.nvim" })
+null_ls.setup({
+	on_attach = function(client, bufnr)
+		null_opts.on_attach(client, bufnr)
 
-	-- MISC HELPERS
-	use({ "kyazdani42/nvim-web-devicons" })
-	use({ "nvim-lua/plenary.nvim" })
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup({})
-		end,
-	})
-	use({ "romainl/vim-cool" })
-	use({
-		"echasnovski/mini.pairs",
-		branch = "stable",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	})
-
-	-- KEYMAP
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup({
-				window = {
-					border = "single", -- none, single, double, shadow
-					position = "bottom",
-					margin = { 0, 0, 0, 0 },
-					padding = { 2, 1, 2, 1 },
-				},
-				layout = {
-					align = "center",
-				},
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
 			})
-		end,
-	})
-
-	-- Bufferline
-	use({
-		"romgrk/barbar.nvim",
-		config = function()
-			require("bufferline").setup({
-				animation = false,
-				auto_hide = false,
-				maximum_length = 35,
-				closable = false,
-				icons = false,
-			})
-		end,
-	})
-
-	-- Statusline
-	use({
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					-- globalstatus = true
-					component_separators = { left = "|", right = "|" },
-					section_separators = { left = "", right = "" },
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = {},
-					lualine_c = { "branch", "diff", "diagnostics" },
-					lualine_x = { "filetype", "encoding" },
-					lualine_y = {},
-					lualine_z = { "location" },
-				},
-			})
-		end,
-	})
-
-	-- Telescope
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({ "nvim-telescope/telescope-file-browser.nvim" })
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		config = function()
-			require("telescope").setup({
-				file_sorter = "",
-				pickers = {
-					find_files = {
-						theme = "ivy",
-					},
-					git_files = {
-						theme = "ivy",
-					},
-					oldfiles = {
-						theme = "ivy",
-					},
-					live_grep = {
-						theme = "ivy",
-					},
-					buffers = {
-						theme = "ivy",
-					},
-					current_buffer_fuzzy_find = {
-						theme = "ivy",
-					},
-					lsp_references = {
-						theme = "ivy",
-					},
-					diagnostics = {
-						theme = "ivy",
-					},
-				},
-				extensions = {
-					file_browser = {
-						theme = "ivy",
-						hidden = false,
-					},
-					fzf = {
-						case_mode = "ignore_case",
-					},
-				},
-			})
-
-			require("telescope").load_extension("file_browser")
-			require("telescope").load_extension("fzf")
-		end,
-	})
-
-	-- Treesitter
-	use({ "windwp/nvim-ts-autotag" })
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = "all",
-				auto_install = true,
-				ignore_install = { "javascript" },
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-				},
-				autotag = {
-					enable = true,
-				},
-			})
-
-			--[[vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-        group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-        callback = function()
-          vim.opt.foldmethod     = 'expr'
-          vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-        end
-      })]]
-			--
-		end,
-	})
-
-	-- Git
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-					change = {
-						hl = "GitSignsChange",
-						text = "│",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
-					delete = {
-						hl = "GitSignsDelete",
-						text = "_",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					topdelete = {
-						hl = "GitSignsDelete",
-						text = "‾",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					changedelete = {
-						hl = "GitSignsChange",
-						text = "~",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
-				},
-				signcolumn = true,
-				current_line_blame = false,
-			})
-		end,
-	})
-	use({ "kdheepak/lazygit.nvim" })
-	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
-
-	-- Coding plugins
-	use({ "neovim/nvim-lspconfig" })
-	use({ "williamboman/mason.nvim" })
-	use({ "williamboman/mason-lspconfig.nvim" })
-	use({ "https://git.sr.ht/~whynothugo/lsp_lines.nvim" })
-	use({ "hrsh7th/nvim-cmp", requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-cmdline" } })
-	use({ "L3MON4D3/LuaSnip", requires = { "saadparwaiz1/cmp_luasnip" } })
-	use({ "jose-elias-alvarez/null-ls.nvim" })
-	use({ "ray-x/go.nvim" })
-
-	-- Writing plugins
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		setup = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	})
-	use({ "folke/zen-mode.nvim" })
-	use({ "dkarter/bullets.vim" })
-	use({ "jose-elias-alvarez/typescript.nvim" })
-
-	if is_bootstrap then
-		require("packer").sync()
-	end
-end)
-
-if is_bootstrap then
-	print("==================================")
-	print("    Plugins are being installed")
-	print("    Wait until Packer completes,")
-	print("       then restart nvim")
-	print("==================================")
-	return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-	command = "source <afile> | PackerCompile",
-	group = packer_group,
-	pattern = vim.fn.expand("$MYVIMRC"),
+		end
+	end,
+	sources = {
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.stylua,
+	},
 })
 
 --[[
 KEYMAPS
 --]]
-
 require("which-key").register({
 	["."] = { "<cmd>Telescope find_files<cr>", "Find file in dir" },
 	["<leader>"] = { "<cmd>Telescope git_files<cr>", "Find git file" },
@@ -370,55 +448,8 @@ require("which-key").register({
 }, { prefix = "<leader>", mode = "n" })
 
 --[[
-OPTIONS
+CUSTOM KEYMAPS (NO PLUGINS)
 --]]
-
-local options = {
-	backup = false, -- creates a backup file
-	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 1, -- more space in the neovim command line for displaying messages
-	completeopt = { "menu", "menuone", "noselect" }, -- mostly just for cmp
-	conceallevel = 0, -- so that `` is visible in markdown files
-	fileencoding = "utf-8", -- the encoding written to a file
-	hlsearch = true, -- highlight all matches on previous search pattern
-	ignorecase = true, -- ignore case in search patterns
-	mouse = "a", -- allow the mouse to be used in neovim
-	pumheight = 10, -- pop up menu height
-	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	smartindent = true,
-	breakindent = true,
-	showtabline = 0, -- always show tabs
-	smartcase = true, -- smart case
-	splitbelow = true, -- force all horizontal splits to go below current window
-	splitright = true, -- force all vertical splits to go to the right of current window
-	swapfile = false, -- creates a swapfile
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	timeoutlen = 200, -- time to wait for a mapped sequence to complete (in milliseconds)
-	undofile = true, -- enable persistent undo
-	updatetime = 230, -- faster completion (4000ms default)
-	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-	expandtab = true, -- convert tabs to spaces
-	shiftwidth = 2, -- the number of spaces inserted for each indentation
-	tabstop = 2, -- insert 2 spaces for a tab
-	cursorline = true, -- highlight the current line
-	number = true, -- set numbered lines
-	relativenumber = true, -- set relative numbered lines
-	numberwidth = 4, -- set number column width to 2 {default 4}
-	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-	linebreak = true,
-	breakat = " ^I!@*-+;:,./?",
-	scrolloff = 6, -- is one of my fav
-	sidescrolloff = 4,
-	winbar = nil,
-}
-
-vim.opt.shortmess:append("c")
-
-for k, v in pairs(options) do
-	vim.opt[k] = v
-end
-
--- Coding (LSP, Formatting, Linting, Completion) is handled in another file
-require("coding")
--- Writing (Journal with mind.nvim, markdown setup)
-require("writing")
+vim.keymap.set("", "<Space>", "<Nop>", opts)
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", opts)
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", opts)
