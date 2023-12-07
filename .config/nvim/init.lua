76af3409a70ddec2ec3e2ee864d24123fa485ccd
Dennis Schoepf <me@dnsc.io>
2023-12-07 19:49:23 +0100

    Adds neovim configuration (besides LSP, linting and formatting)

---
+++ .config/nvim/init.lua
@@ -0,0 +1,19 @@
+--[[
+███╗   ██╗██╗   ██╗██╗███╗   ███╗
+████╗  ██║██║   ██║██║████╗ ████║
+██╔██╗ ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+
+Config Author: Dennis Schoepf
+Version: 1.0.0
+]]
+--
+
+require("dnsc.leader")
+require("dnsc.options")
+require("dnsc.utils")
+require("dnsc.keymaps")
+require("dnsc.lazy")
+require("dnsc.plugins")
---
+++ .config/nvim/lua/dnsc/plugins.lua
@@ -0,0 +1,39 @@
+local plugins = {
+	-- Utils
+	"nvim-lua/plenary.nvim",
+	"folke/which-key.nvim",
+	-- Appearance
+	"kyazdani42/nvim-web-devicons",
+	{ "catppuccin/nvim", name = "catppuccin" },
+	-- Helpers
+	"romainl/vim-cool",
+	"echasnovski/mini.pairs",
+	"echasnovski/mini.starter",
+	"echasnovski/mini.comment",
+	"tpope/vim-sleuth",
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			},
+		},
+	},
+	-- File/Folder Management
+	"stevearc/oil.nvim",
+	-- Treesitter (and TS-based plugins)
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	-- Git
+	"kdheepak/lazygit.nvim",
+	"lewis6991/gitsigns.nvim",
+	-- Bufferline
+	"romgrk/barbar.nvim",
+	-- Statusline
+	"nvim-lualine/lualine.nvim",
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
