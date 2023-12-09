1df66612e2658e8e837b47621c9aaac6830dad63
Dennis Schoepf <me@dnsc.io>
2023-12-09 16:21:18 +0100

    Removes unused config files

--- .config/nvim/after/plugin/barbar.lua
+++
@@ -1,7 +0,0 @@
-require("bufferline").setup({
-	animation = false,
-	auto_hide = false,
-	maximum_length = 35,
-	icons = { filetype = { enabled = false }, button = "", modified = { button = "m" } },
-	highlight_visible = false,
-})
