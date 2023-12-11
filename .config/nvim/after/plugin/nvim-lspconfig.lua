9398a0cbba18e0a87be096a8e249384190f7077f
Dennis Schoepf <me@dnsc.io>
2023-12-11 12:56:19 +0100

    Adds sonarlint to nvim

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -81,17 +81,23 @@ lspconfig.tailwindcss.setup({
 require("sonarlint").setup({
   server = {
     cmd = {
-      "sonarlint-language-server",
+      "java",
+      "-jar",
+      vim.fn.expand("~/.sonarlint/sonarlint-ls.jar"),
       -- Ensure that sonarlint-language-server uses stdio channel
       "-stdio",
       "-analyzers",
-      -- paths to the analyzers you need, using those for python and java in this example
-      vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-      vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-      vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+      vim.fn.expand("~/.sonarlint/sonarjs.jar"),
+      vim.fn.expand("~/.sonarlint/sonarhtml.jar"),
     },
   },
-  filetypes = { "typescript" },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "typescriptreact",
+    "javascriptreact",
+    "html",
+  },
 })
 
 -- LSP Keybindings
