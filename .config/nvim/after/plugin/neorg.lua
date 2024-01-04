5a0697c1fee94dadc46b932e0474daf3eb357deb
Dennis Schoepf <me@dnsc.io>
2024-01-04 17:56:46 +0100

    Adds neorg configuration in separate file

---
+++ .config/nvim/after/plugin/neorg.lua
@@ -0,0 +1,40 @@
+require("neorg").setup({
+  load = {
+    ["core.defaults"] = {},
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          work = "~/notes/work",
+          personal = "~/notes/personal",
+          memex = "~/notes/memex",
+        },
+      },
+    },
+    ["core.journal"] = {
+      config = {
+        journal_folder = "dailies",
+      },
+    },
+    ["core.completion"] = {
+      config = {
+        engine = "nvim-cmp",
+      },
+    },
+    ["core.concealer"] = {},
+    ["core.export"] = {},
+    ["core.export.markdown"] = {},
+    ["core.presenter"] = {
+      config = {
+        zen_mode = "zen-mode",
+      },
+    },
+    ["core.ui.calendar"] = {},
+    ["core.keybinds"] = {
+      config = {
+        hook = function(keybinds)
+          keybinds.map("norg", "n", "<Leader>oec", "core.looking-glass.magnify-code-block")
+        end,
+      },
+    },
+  },
+})
