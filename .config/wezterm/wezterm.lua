c815f18a843f3bccc3fb0ae7db59f015f67cc1c3
Dennis Schoepf <me@dnsc.io>
2024-01-31 15:27:56 +0100

    Adapts wezterm bg opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,47 +1,47 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local act = wezterm.action
 local hn = wezterm.hostname()
 local config = {}
 
 if wezterm.config_builder then
-  ---@diagnostic disable-next-line: lowercase-global
-  config = wezterm.config_builder()
+	---@diagnostic disable-next-line: lowercase-global
+	config = wezterm.config_builder()
 end
 
 -- Helper
 local function get_padding(padding)
-  return {
-    left = padding,
-    right = padding,
-    top = padding,
-    bottom = padding - 10,
-  }
+	return {
+		left = padding,
+		right = padding,
+		top = padding,
+		bottom = padding - 10,
+	}
 end
 
 -- General Options
 
 -- Appearance
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = "catppuccin-mocha"
 config.window_decorations = "NONE"
 config.enable_tab_bar = false
 config.window_padding = get_padding(25)
-config.window_background_opacity = 0.94
+config.window_background_opacity = 0.975
 config.colors = { foreground = "white" }
 
 -- Overrides
 if hn == "contraption.digital-h.de" then
-  config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
-  config.font_size = 17.5
-  config.window_decorations = "RESIZE"
-  config.window_padding = get_padding(30)
+	config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+	config.font_size = 17.5
+	config.window_decorations = "RESIZE"
+	config.window_padding = get_padding(30)
 end
 
 -- Keybindings
 config.keys = {
-  { key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
-  { key = "c", mods = "SUPER", action = act.CopyTo("ClipboardAndPrimarySelection") }
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "c", mods = "SUPER", action = act.CopyTo("ClipboardAndPrimarySelection") },
 }
 
 return config
